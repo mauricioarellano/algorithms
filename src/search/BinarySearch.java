340d72e2146c79c7abc192fef34cdc142bd8c971
Mau Arellano <mac_corp@hotmail.com>
2016-12-14 17:59:31 -0600

    binary search skeleton

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -2,9 +2,18 @@ package search;
 
 public class BinarySearch {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int search(int[] array, int value){
+		int index = -1;
+		int begin =0;
+		int end = array.length-1;
+		int half = (end-begin)/2;
+		
+		if(array[half]==value){
+			return half;
+		} else if(array[half]<value){
+			
+		}
+		
+		return index;
 	}
-
 }
