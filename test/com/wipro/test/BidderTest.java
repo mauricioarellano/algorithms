9c7ec63e522c132a278fd6cdac0c4b147714021c
Mau Arellano <mac_corp@hotmail.com>
2017-07-22 00:01:03 -0500

    TestCase for Bidder fixed

--- test/com/wipro/test/BidderTest.java
+++ test/com/wipro/test/BidderTest.java
@@ -9,49 +9,51 @@ import com.wipro.test.exception.MaxBidReachedException;
 
 public class BidderTest {
 	
-	public static int maxIncrement(Bidder bidder){
-		int bid = 0;
-		while(bid < 240){
-			int nexBid;
-			try {
-				nexBid = bidder.incrementBid();
-			} catch (MaxBidReachedException e) {
-				break;
-			}
-			bid=nexBid;
-		}
-		System.out.println("Bid: " + bid);
-		return bid;
-	}
-
 	@Test
-	public void testMaxIncrement4LindaRecordPlayer() {
-		int initial = 170;
-		int maxBid = 240;
-		int increment = 3;
-		Bidder bidder = new Bidder("", new Offer(initial, maxBid, increment));
-		int bid = maxIncrement(bidder);
-		Assert.assertTrue(bid<=maxBid && bid>(maxBid-increment));
-	}
-	
-	@Test
-	public void testMaxIncrement4DaveRecordPlayer() {
-		int initial = 160;
-		int maxBid = 243;
-		int increment = 7;
-		Bidder bidder = new Bidder("", new Offer(initial, maxBid, increment));
-		int bid = maxIncrement(bidder);
-		Assert.assertTrue(bid<=maxBid && bid>(maxBid-increment));
-	}
-
-	@Test
-	public void testMaxIncrement4EricRecordPlayer() {
+	public void testValidIncrement() {
 		int initial = 190;
 		int maxBid = 240;
 		int increment = 4;
-		Bidder bidder = new Bidder("", new Offer(initial, maxBid, increment));
-		int bid = maxIncrement(bidder);
-		Assert.assertTrue(bid<=maxBid && bid>(maxBid-increment));
+		Bidder bidder = new Bidder("Test", new Offer(initial, maxBid, increment));
+		try {
+			bidder.incrementBid();
+		} catch (MaxBidReachedException e) {
+			// TODO Auto-generated catch block
+			Assert.fail(e.getMessage());
+		}
+		System.out.println(bidder);
+		Assert.assertEquals(194, bidder.getBid());
+	}
+	
+	@Test
+	public void testValidMaximIncrement() {
+		int initial = 190;
+		int maxBid = 200;
+		int increment = 10;
+		Bidder bidder = new Bidder("Test", new Offer(initial, maxBid, increment));
+		try {
+			bidder.incrementBid();
+		} catch (MaxBidReachedException e) {
+			// TODO Auto-generated catch block
+			Assert.fail(e.getMessage());
+		}
+		System.out.println(bidder);
+		Assert.assertEquals(maxBid, bidder.getBid());
+	}
+	
+	@Test
+	public void testInvalidIncrement() {
+		int initial = 190;
+		int maxBid = 200;
+		int increment = 11;
+		Bidder bidder = new Bidder("Test", new Offer(initial, maxBid, increment));
+		try {
+			bidder.incrementBid();
+		} catch (MaxBidReachedException e) {
+			System.out.println("maxBid reached without increment");
+		}
+		System.out.println(bidder);
+		Assert.assertEquals(190, bidder.getBid());
 	}
 
 }
