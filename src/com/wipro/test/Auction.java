29ae1e49ce487eacaaa8d99012f6ee78c055695a
Mau Arellano <mac_corp@hotmail.com>
2017-07-23 13:35:55 -0500

    refactored API

--- src/com/wipro/test/Auction.java
+++ src/com/wipro/test/Auction.java
@@ -7,32 +7,22 @@ import com.wipro.test.exception.MaxBidReachedException;
 
 public class Auction  {
 	private String item;
-	private List<Bidder> bidders;
 		
-	public Auction() {
-		this.bidders = new ArrayList<Bidder>();
-	}
-	
 	public Auction(String item) {
-		this();
 		this.item = item;
 	}
 	
-	public void addBidder(Bidder bidder){
-		bidders.add(bidder);
-	}
-	
-	public Bidder executeAuction(){
-		if(bidders.isEmpty()){
+	public Bidder executeAuction(List<Bidder> biddersList){
+		if(biddersList==null || biddersList.isEmpty()){
 			return null;
 		}
 		
 		int currentBid = 0;
 		Bidder currentBidder = null;
 		
-		while(bidders.size()>1){
-			List<Bidder> biddersAux = new ArrayList<Bidder>(bidders);
-			for (Bidder bidder : bidders) {
+		while(biddersList.size()>1){
+			List<Bidder> biddersAux = new ArrayList<Bidder>(biddersList);
+			for (Bidder bidder : biddersList) {
 				try {
 					if(currentBid < bidder.getBid()){
 						currentBid = bidder.getBid();
@@ -40,22 +30,21 @@ public class Auction  {
 					} else if(currentBid >= bidder.getBid() && !bidder.equals(currentBidder)){
 						if(currentBid > bidder.getOffer().getMaxBid()){
 							throw new MaxBidReachedException();
-						} 
+						}
 						bidder.incrementBid();
 						if(currentBid < bidder.getBid()){
 							currentBid = bidder.getBid();
 							currentBidder = bidder;
 						}
 					}
-					
 				} catch (MaxBidReachedException e) {
 					biddersAux.remove(bidder);
 				}
 			}
-			bidders = biddersAux;
+			biddersList = biddersAux;
 		}
 		
-		return bidders.get(0);
+		return biddersList.get(0);
 	}
 
 	@Override
--- test/com/wipro/test/AuctionTest.java
+++ test/com/wipro/test/AuctionTest.java
@@ -2,6 +2,9 @@ package com.wipro.test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class AuctionTest {
@@ -12,12 +15,13 @@ public class AuctionTest {
 		Bidder linda = new Bidder("Linda", new Offer(170, 240, 3));
 		Bidder dave =  new Bidder("Dave",  new Offer(160, 243, 7));
 		Bidder eric =  new Bidder("Eric",  new Offer(190, 240, 4));
+		List<Bidder> bidders = new ArrayList<Bidder>();
 		
-		auction.addBidder(linda);
-		auction.addBidder(dave);
-		auction.addBidder(eric);
+		bidders.add(linda);
+		bidders.add(dave);
+		bidders.add(eric);
 		
-		Bidder winner = auction.executeAuction();
+		Bidder winner = auction.executeAuction(bidders);
 		System.out.println("== " + auction + " winner is: " + winner.toString() + " ==\n");
 		assertNotNull(winner);
 		assertEquals(linda, winner);
@@ -29,12 +33,13 @@ public class AuctionTest {
 		Bidder linda = new Bidder("Linda", new Offer(30, 70, 4));
 		Bidder dave =  new Bidder("Dave",  new Offer(30, 70, 3));
 		Bidder eric =  new Bidder("Eric",  new Offer(40, 90, 2));
+		List<Bidder> bidders = new ArrayList<Bidder>();
 		
-		auction.addBidder(linda);
-		auction.addBidder(dave);
-		auction.addBidder(eric);
+		bidders.add(linda);
+		bidders.add(dave);
+		bidders.add(eric);
 		
-		Bidder winner = auction.executeAuction();
+		Bidder winner = auction.executeAuction(bidders);
 		System.out.println("== " + auction + " winner is: " + winner.toString() + " ==\n");
 		assertNotNull(winner);
 		assertEquals(eric, winner);
@@ -47,14 +52,33 @@ public class AuctionTest {
 		Bidder dave =  new Bidder("Dave",  new Offer(10_000, 70_000, 15_000));
 		Bidder eric =  new Bidder("Eric",  new Offer(22_000, 70_000,  8_000));
 		
-		auction.addBidder(linda);
-		auction.addBidder(dave);
-		auction.addBidder(eric);
+		List<Bidder> bidders = new ArrayList<Bidder>();
 		
-		Bidder winner = auction.executeAuction();
+		bidders.add(linda);
+		bidders.add(dave);
+		bidders.add(eric);
+		
+		Bidder winner = auction.executeAuction(bidders);
 		System.out.println("== " + auction + " winner is: " + winner.toString() + " ==\n");
 		assertNotNull(winner);
 		assertEquals(dave, winner);
+	}
+	
+	@Test
+	public void test0() {
+		Auction auction = new Auction("Zero");
+		List<Bidder> bidders = new ArrayList<Bidder>();
+		Bidder winner = auction.executeAuction(bidders);
+		System.out.println("== " + auction + " winner is: " + winner + " ==\n");
+		assertNull(winner);
+	}
+
+	@Test
+	public void testNull() {
+		Auction auction = new Auction("Null");
+		Bidder winner = auction.executeAuction(null);
+		System.out.println("== " + auction + " winner is: " + winner + " ==\n");
+		assertNull(winner);
 	}
 
 }
