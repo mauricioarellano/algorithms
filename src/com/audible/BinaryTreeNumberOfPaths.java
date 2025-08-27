3703636eb02b1abdfc470d24e18dfa8ce9aede20
Mauricio Arellano <mauricioarellano@users.noreply.github.com>
2025-08-27 12:28:05 -0600

    Create BinaryTreeNumberOfPaths.java

---
+++ src/com/audible/BinaryTreeNumberOfPaths.java
@@ -0,0 +1,23 @@
+/*
+Given a binary tree, find the number of paths in the binary tree which has a sum K. A path can start at any node 
+and end at any node. The path can only travel downwards.
+
+
+K = 3
+     
+
+      3
+     /\
+    2  1
+   /\   \
+  1  1   1
+          \
+           1
+
+
+           Output: 4
+           3
+           2->1
+           2->1
+           1->1->1
+*/
